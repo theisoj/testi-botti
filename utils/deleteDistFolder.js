7c5256072a5b98cd4f4a3e624166c3e39de1571c
Jesse Keskelä <keskelajesse@gmail.com>
2023-03-24 17:25:27 +0200

    Korjattu.

--- utils/deleteDistFolder.js
+++ utils/deleteDistFolder.js
@@ -1,14 +1,8 @@
 import fs from "fs/promises"
 
 async function main() {
-    const folder = await fs.rm("./dist", { recursive: true, force: true })
-
-    if (folder == null) {
-        console.error("Kansiota ei ole olemassa. Oletko poistanut sen jo?")
-        process.exit(1)
-    } else {
-        console.log("dist-kansio poistettu.")
-    }
+    await fs.rm("./dist", { recursive: true, force: true })
+    console.log("dist-kansio poistettu.")
 }
 
 main()
